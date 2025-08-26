d16aaed10ecbfac2cac22ab521fb532c725d34b5
Camilo Alejandro <79614560+its-camilo@users.noreply.github.com>
2025-08-26 19:04:53 +0000

    menu modular

---
+++ app/components/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Components exports
+ */
+
+// Main components
+export { Sidebar, SIDEBAR_WIDTH } from "./Sidebar";
+export { Collapsible } from "./Collapsible";
+export { ExternalLink } from "./ExternalLink";
+export { HapticTab } from "./HapticTab";
+export { HelloWave } from "./HelloWave";
+export { ThemedText } from "./ThemedText";
+export { ThemedView } from "./ThemedView";
+
+// Auth components
+export * from "./Auth";
+
+// Keep alive components
+export * from "./keepalive";
